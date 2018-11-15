eabb3820bd86d684d82ec177757e96e9b40b203d
yhasib6487 <33043549+yhasib6487@users.noreply.github.com>
2018-11-15 13:36:31 -0500

    Delete Main.java

--- LinearEquation/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main( String[] args ) {
-        QuadraticEquation myEq = new QuadraticEquation(1, 2, 4);
-        Coords vertex = myEq.vertex();
-        QuadraticRoots roots = myEq.getRoots();
-        System.out.println(vertex);
-        System.out.println(roots);
-    }
-}
